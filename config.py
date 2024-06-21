e653fd8bea03c72cb1def9bc208f754d1b5f339b
Joonbeom Kwon <djbkwon@gmail.com>
2024-06-20 19:10:26 -0700

    run through karina photos

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import logging
 
 # Constants
 BASE_IMG_DB_NAME = "face-db"
-BASE_IMG_DIR_NAME = "chaewon"
-BASE_IMG_FILE_NAME = "2023-09-28_16-00-46_UTC_8.jpg"
+BASE_IMG_DIR_NAME = "karina"
+BASE_IMG_FILE_NAME = ""
 
-REFERENCE_IMG_FILE_NAME = "reference_a.jpg"  # Faces will be compared against this image, which therefore must be high quality.
+REFERENCE_IMG_FILE_NAME = "2023-05-25_05-27-25_UTC_1.jpg"  # Faces will be compared against this image, which therefore must be high quality.
 
 CROPPED_IMG_DB_NAME = "cropped-face-db"
 
